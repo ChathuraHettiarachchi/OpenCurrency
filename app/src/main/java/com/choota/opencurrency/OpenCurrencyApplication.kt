59c23750091b379a82ff9c5c652a3447e69f494b
Chathura <chathura93@yahoo.com>
2022-11-27 00:55:25 +0530

    pref included

--- app/src/main/java/com/choota/opencurrency/OpenCurrencyApplication.kt
+++ app/src/main/java/com/choota/opencurrency/OpenCurrencyApplication.kt
@@ -1,11 +1,13 @@
 package com.choota.opencurrency
 
 import android.app.Application
+import com.chibatching.kotpref.Kotpref
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class OpenCurrencyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+        Kotpref.init(applicationContext)
     }
 }
